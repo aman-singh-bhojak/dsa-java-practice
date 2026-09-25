ace09f53d4ad0cf90c53513c4fb28292461f3a14
Aman Singh Bhojak <amanbhojak.tech@gmail.com>
2026-09-25 18:40:15 +0530

    feat: solve #217 Contains Duplicate

---
+++ Leetcode/Array_/_0217_Contains_Duplicate.java
@@ -0,0 +1,27 @@
+package Leetcode.Array_;
+
+import java.util.Arrays;
+
+public class _0217_Contains_Duplicate {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println(containsDuplicate(nums1)); 
+        System.out.println(containsDuplicate(nums2)); 
+        System.out.println(containsDuplicate(nums3)); 
+    }
+
+    static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
